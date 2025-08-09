7aee78f19bad73de20836eda17a37e3640938c21
MexJuan40312 <robertukisimon@hotmail.com>
2025-08-09 14:40:49 -0600

    Estructuración base del proyecto

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl display flex justify-center'>Hola mundo desde React</h1>
+    </>
+  )
+}
+
+export default App
